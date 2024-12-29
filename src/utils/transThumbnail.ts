691ea5c14bb0f8bc9e255f1069f9996fdf456910
ijimnlosk <skek298@naver.com>
2024-12-29 16:19:48 +0900

    📂docs: 주석 제거

--- src/utils/transThumbnail.ts
+++ src/utils/transThumbnail.ts
@@ -1,12 +1,11 @@
 export const transThumbnail = (
   thumbnail: {medium?: {url: string}; url?: string} | {url: string},
 ): string => {
-  // url이 있는 경우 반환, medium?.url이 있는 경우 반환
   if ('url' in thumbnail && thumbnail.url) {
     return thumbnail.url;
   }
   if ('medium' in thumbnail && thumbnail.medium?.url) {
     return thumbnail.medium.url;
   }
-  throw new Error('Invalid thumbnail format'); // 에러 처리
+  throw new Error('Invalid thumbnail format');
 };
