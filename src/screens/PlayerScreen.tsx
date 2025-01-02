b533c5b054d8cb1f1d026f04b41d1df6c00e60f7
ijimnlosk <skek298@naver.com>
2025-01-02 16:07:26 +0900

    🟢feat: 현재 재생 시간 추가

--- src/screens/PlayerScreen.tsx
+++ src/screens/PlayerScreen.tsx
@@ -4,31 +4,40 @@ import {useYouTubePlayer} from '../hooks/useYouTubePlayer';
 import {getVideoId} from '../utils/getVideoId';
 import YouTubePlayer from 'react-native-youtube-iframe';
 import {PlayerScreenProps} from './type';
-import {postLatestList} from '../apis/latestListAPI';
-import {useQueryClient} from '@tanstack/react-query';
 import BookMark from '../components/BookMark';
 import {transThumbnail} from '../utils/transThumbnail';
+import {useNavigation} from '@react-navigation/native';
+import {usePostLatestList} from '../hooks/usePostLatestList';
 
 const PlayerScreen = ({route}: PlayerScreenProps) => {
   const {album} = route.params;
   const videoId = getVideoId(album.id);
-  const {isPlaying, handleStateChange, togglePlay} = useYouTubePlayer();
-  const queryClient = useQueryClient();
+  const {isPlaying, handleStateChange, togglePlay, currentTime} =
+    useYouTubePlayer();
+  const navigation = useNavigation();
+  const postLatest = usePostLatestList(videoId);
 
   const handlePlay = async () => {
     togglePlay();
     if (!isPlaying) {
-      await postLatestList(videoId);
-      queryClient.invalidateQueries({queryKey: ['latestList']});
-      queryClient.invalidateQueries({queryKey: ['markList']});
-      queryClient.refetchQueries({queryKey: ['latestList']});
-      queryClient.refetchQueries({queryKey: ['markList']});
+      await postLatest();
+    }
+  };
+
+  const handleEnd = () => {
+    navigation.goBack();
+  };
+
+  const handleStateChangeWithEnd = (state: string) => {
+    handleStateChange(state);
+    if (state === 'ended') {
+      handleEnd();
     }
   };
 
   return (
     <View className="flex-1 justify-center items-center p-4 mt-40">
-      <View className="flex justify-center items-center h-[300px]">
+      <View className=" flex justify-center items-center h-[300px]">
         <Image
           source={{uri: transThumbnail(album.thumbnail)}}
           className="w-[200px] h-[200px] mb-5"
@@ -38,14 +47,19 @@ const PlayerScreen = ({route}: PlayerScreenProps) => {
         </Text>
         <Text className="text-xl text-#cccccc">{album.channelTitle}</Text>
         <BookMark album={album} />
+        <View className="py-10">
+          <Text>{`${Math.floor(currentTime / 60)}:${Math.floor(
+            currentTime % 60,
+          )}`}</Text>
+        </View>
+        <Button title={isPlaying ? 'Pause' : 'Play'} onPress={handlePlay} />
         <YouTubePlayer
           videoId={videoId}
           height={250}
           play={isPlaying}
-          onChangeState={handleStateChange}
+          onChangeState={handleStateChangeWithEnd}
         />
       </View>
-      <Button title={isPlaying ? 'Pause' : 'Play'} onPress={handlePlay} />
     </View>
   );
 };
