6497085bc22cf51ec8bd82aa9ed93889520ecd64
55555-Jyeon <jyeon380516@gmail.com>
2025-02-03 23:44:59 +0900

    🎁 styles: 자주 사용하는 스타일 정리

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './App.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './styles/align.css',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        system: {
+          white: '#ffffff',
+          black: '#000000',
+        },
+        grey: {
+          10: '#F8FAFC',
+          20: '#E2E8F0',
+          30: '#CBD5E1',
+          40: '#94A3B8',
+          50: '#64748B',
+          60: '#475569',
+          70: '#334155',
+          80: '#1E293B',
+          90: '#0F172A',
+        },
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
